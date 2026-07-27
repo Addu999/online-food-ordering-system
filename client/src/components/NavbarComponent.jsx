c91241cb15138a0ae85c1da9b4bfca02a838528c
Addu999 <addu9822@gmail.com>
2026-07-28 02:08:15 +0530

    add navbar links for login and register

--- client/src/components/NavbarComponent.jsx
+++ client/src/components/NavbarComponent.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+
 function Navbar() {
   return (
     <nav
@@ -13,22 +14,16 @@ function Navbar() {
     >
       <h2>🍔 Foodie</h2>
 
-      <div>
-       <Link to="/" style={{ color: "white", marginRight: "20px" }}>
-  Home
-</Link>
-
-       <Link to="/menu" style={{ color: "white", marginRight: "20px" }}>
-  Menu
-</Link>
-
-        <Link to="/cart" style={{ color: "white", marginRight: "20px" }}>
-  Cart
-</Link>
-
-        <Link to="/login" style={{ color: "white" }}>
-  Login
-</Link>
+      <div style={{ display: "flex", gap: "20px", alignItems: "center" }}>
+        <Link to="/" style={{ color: "white", textDecoration: "none", fontWeight: "bold" }}>
+          Home
+        </Link>
+        <Link to="/login" style={{ color: "white", textDecoration: "none", fontWeight: "bold" }}>
+          Login
+        </Link>
+        <Link to="/register" style={{ backgroundColor: "white", color: "#ff4d4d", padding: "8px 15px", borderRadius: "5px", textDecoration: "none", fontWeight: "bold" }}>
+          Register
+        </Link>
       </div>
     </nav>
   );
